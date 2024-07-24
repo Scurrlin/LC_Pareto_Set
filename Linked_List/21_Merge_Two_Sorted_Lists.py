bb353ae5c848364f2faeb586f4b0bb9efee659b4
Sean Currlin <sean.currlin@gmail.com>
2024-07-24 16:15:40 -0400

    Add Linked_List problems

---
+++ Linked_List/21_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,19 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        temp = ListNode(0)
+        current = temp
+        while l1 and l2:
+            if l1.val <= l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+        current.next = l1 if l1 else l2
+        return temp.next
