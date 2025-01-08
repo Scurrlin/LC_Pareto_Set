02690ad061002b768692708eeaef6b89a638d6ea
Sean Currlin <sean.currlin@gmail.com>
2025-01-08 09:18:03 -0500

    update solution for 36

--- Arrays&Hashing/36_Valid_Sudoku.py
+++ Arrays&Hashing/36_Valid_Sudoku.py
@@ -1,22 +1,24 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         b = board
-        cols = collections.defaultdict(set)
         rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
 
         for r in range(9):
             for c in range(9):
                 if b[r][c] == ".":
                     continue
+                
                 if (
                     b[r][c] in rows[r]
                     or b[r][c] in cols[c]
-                    or b[r][c] in squares[(r // 3, c // 3)]
+                    or b[r][c] in squares[(r//3, c//3)]
                 ):
                     return False
-                cols[c].add(b[r][c])
+                
                 rows[r].add(b[r][c])
-                squares[(r // 3, c // 3)].add(b[r][c])
-
+                cols[c].add(b[r][c])
+                squares[(r//3,c //3)].add(b[r][c])
+        
         return True
