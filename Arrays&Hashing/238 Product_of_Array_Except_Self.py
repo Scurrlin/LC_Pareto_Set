52140922c4794ae1ff7a14e628ab6a642b1541e6
Sean Currlin <sean.currlin@gmail.com>
2025-01-09 08:42:47 -0500

    update solution for 238

--- Arrays&Hashing/238 Product_of_Array_Except_Self.py
+++ Arrays&Hashing/238 Product_of_Array_Except_Self.py
@@ -1,16 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-
-        prefix = [1] * n
-        suffix = [1] * n
+        p = [1] * n
+        s = [1] * n
 
         for i in range(1, n):
-            prefix[i] = prefix[i - 1] * nums[i - 1]
+            p[i] = p[i - 1] * nums[i - 1]
         
         for i in range(n - 2, -1, -1):
-            suffix[i] = suffix[i + 1] * nums[i + 1]
+            s[i] = s[i + 1] * nums[i + 1]
         
-        answer = [prefix[i] * suffix[i] for i in range(n)]
+        answer = [p[i] * s[i] for i in range(n)]
 
         return answer
