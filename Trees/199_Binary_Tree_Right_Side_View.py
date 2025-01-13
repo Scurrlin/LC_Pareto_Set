8ad1564a66150637cbba9026ba2eaab93d5a71a1
Sean Currlin <sean.currlin@gmail.com>
2025-01-13 08:20:42 -0500

    update solution for 199

--- Trees/199_Binary_Tree_Right_Side_View.py
+++ Trees/199_Binary_Tree_Right_Side_View.py
@@ -1,20 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def rightSideView(self, root: TreeNode) -> list[int]:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return []
-        
-        right_view = []
-        queue = deque([root])
-        
-        while queue:
-            level_length = len(queue)
-            for i in range(level_length):
-                node = queue.popleft()
-                if i == level_length - 1:
-                    right_view.append(node.val)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-        
-        return right_view
+        view = []
+        def dfs(node, level):
+            if node:
+                if level == len(view):
+                    view.append(node.val)
+                dfs(node.right, level + 1)
+                dfs(node.left, level + 1)
+                
+        dfs(root, 0)
+        return view
