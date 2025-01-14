39bdf11b523d9573a8deee7b8add074b2135d217
Sean Currlin <sean.currlin@gmail.com>
2025-01-14 09:30:15 -0500

    update solution for 20

--- Stack/20_Valid_Parentheses.py
+++ Stack/20_Valid_Parentheses.py
@@ -1,12 +1,14 @@
 class Solution:
     def isValid(self, s):
         stack = []
-        bracket_map = {")": "(", "}": "{", "]": "["}
+        b_map = {")": "(", "}": "{", "]": "["}
         for char in s:
-            if char in bracket_map:
+            if char in b_map:
                 top_element = stack.pop() if stack else '#'
-                if bracket_map[char] != top_element:
+                if b_map[char] != top_element:
                     return False
             else:
                 stack.append(char)
         return not stack
+
+# b_map = bracket_map
