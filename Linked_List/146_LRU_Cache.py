8d9103cc237b078bb1d04159013c9ac4ca2a9847
Sean Currlin <sean.currlin@gmail.com>
2025-01-14 09:26:34 -0500

    update solution for 146

--- Linked_List/146_LRU_Cache.py
+++ Linked_List/146_LRU_Cache.py
@@ -1,43 +1,21 @@
-class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
-
+import collections
 
 class LRUCache:
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}  # map key to node
-
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
-
-    # remove node from list
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-
-    # insert node at right
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        self.capacity = capacity
+        self.dic = collections.OrderedDict()
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
+        if key not in self.dic:
+            return -1
+
+        self.dic.move_to_end(key)
+        return self.dic[key]
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+        if key in self.dic:
+            self.dic.move_to_end(key)
 
-        if len(self.cache) > self.cap:
-            # remove from the list and delete the LRU from hashmap
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
+        self.dic[key] = value
+        if len(self.dic) > self.capacity:
+            self.dic.popitem(False)
