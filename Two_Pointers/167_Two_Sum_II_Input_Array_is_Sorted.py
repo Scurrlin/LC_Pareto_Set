105a9dd0cd03eef64bda039983776499b7d16f2e
Sean Currlin <sean.currlin@gmail.com>
2025-01-20 18:12:38 -0500

    update solution for 167

--- Two_Pointers/167_Two_Sum_II_Input_Array_is_Sorted.py
+++ Two_Pointers/167_Two_Sum_II_Input_Array_is_Sorted.py
@@ -1,14 +1,15 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        L = 0
-        R = len(numbers) - 1
+        n, t = numbers, target
+        l = 0
+        r = len(n) - 1
 
-        while L < R:
-            total = numbers[L] + numbers[R]
+        while l < r:
+            total = n[l] + n[r]
 
-            if total == target:
-                return [L + 1, R + 1]
-            elif total > target:
-                R -= 1
+            if total == t:
+                return[l + 1, r + 1]
+            elif total > t:
+                r -= 1
             else:
-                L += 1
+                l += 1
