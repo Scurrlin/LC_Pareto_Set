f3b0feda5f473682a00fb219c1a2b07aa8dad847
Sean Currlin <sean.currlin@gmail.com>
2025-01-20 14:47:18 -0500

    update solution for 15

--- Two_Pointers/15_3Sum.py
+++ Two_Pointers/15_3Sum.py
@@ -1,27 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
-        nums.sort()
+        n = nums
+        n.sort()
 
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
+        for i in range(len(n)):
+            if i > 0 and n[i] == n[i - 1]:
                 continue
             
             j = i + 1
-            k = len(nums) - 1
+            k = len(n) - 1
 
             while j < k:
-                total = nums[i] + nums[j] + nums[k]
+                total = n[i] + n[j] + n[k]
 
                 if total > 0:
                     k -= 1
                 elif total < 0:
                     j += 1
                 else:
-                    res.append([nums[i], nums[j], nums[k]])
+                    res.append([n[i], n[j], n[k]])
                     j += 1
 
-                    while nums[j] == nums[j-1] and j < k:
+                    while n[j] == n[j - 1] and j < k:
                         j += 1
         
         return res
