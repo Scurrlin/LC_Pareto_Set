483b61a6e9f754e1b9a7a245cabc35dfbe3ff656
Sean Currlin <sean.currlin@gmail.com>
2025-02-03 11:37:51 -0500

    update solution for 226

--- Trees/226_Invert_Binary_Tree.py
+++ Trees/226_Invert_Binary_Tree.py
@@ -3,10 +3,8 @@ class Solution:
         if not root:
             return None
         
-        # swap the children
         root.left, root.right = root.right, root.left
         
-        # make 2 recursive calls
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
