757e8adc1bbc254aae3a76fac6a7e99ca368f741
Sean Currlin <sean.currlin@gmail.com>
2025-02-10 12:03:40 -0500

    update solution for 36

--- Arrays&Hashing/Solutions/36_Valid_Sudoku.py
+++ Arrays&Hashing/Solutions/36_Valid_Sudoku.py
@@ -8,19 +8,16 @@ class Solution:
         for r in range(9):
             for c in range(9):
                 if b[r][c] == ".":
-                    continue
-                
+                    continue               
                 if (
                     b[r][c] in rows[r]
                     or b[r][c] in cols[c]
                     or b[r][c] in squares[(r//3, c//3)]
                 ):
                     return False
-                
                 rows[r].add(b[r][c])
                 cols[c].add(b[r][c])
                 squares[(r//3, c//3)].add(b[r][c])
-        
         return True
 
 # Time Complexity: O(1)
--- Arrays&Hashing/Stencils/36_Valid_Sudoku.md
+++ Arrays&Hashing/Stencils/36_Valid_Sudoku.md
@@ -10,18 +10,15 @@ class Solution:
             f c i r(9):
                 i b[r][c] == ".":
                     c
-                
                 i (
                     b[r][c] i r[r]
                     o b[r][c] i c[c]
                     o b[r][c] i s[(r//3, c//3)]
                 ):
                     r F
-                
                 r[r].a(b[r][c])
                 c[c].a(b[r][c])
                 s[(r//3, c//3)].a(b[r][c])
-        
         r T
 
 # Time Complexity: O(1)
