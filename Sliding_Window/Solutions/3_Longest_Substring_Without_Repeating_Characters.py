4170402d8eb9a7ce108b79e63753709143bb3ad4
Sean Currlin <sean.currlin@gmail.com>
2025-10-29 10:14:54 -0400

    update Sliding_Window solutions

--- Sliding_Window/Solutions/3_Longest_Substring_Without_Repeating_Characters.py
+++ Sliding_Window/Solutions/3_Longest_Substring_Without_Repeating_Characters.py
@@ -1,8 +1,7 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         charSet = set()
-        l = 0
-        res = 0
+        l, res = 0, 0
 
         for r in range(len(s)):
             while s[r] in charSet:
--- Sliding_Window/Solutions/424_Longest_Repeating_Character_Replacement.py
+++ Sliding_Window/Solutions/424_Longest_Repeating_Character_Replacement.py
@@ -1,13 +1,12 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
-        l = 0
-        maxfreq = 0
+        l, maxFreq = 0, 0
 
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
-            maxfreq = max(maxfreq, count[s[r]])
-            if (r - l + 1) - maxfreq > k:
+            maxFreq = max(maxFreq, count[s[r]])
+            if (r - l + 1) - maxFreq > k:
                 count[s[l]] -= 1
                 l += 1
         return (r - l + 1)
